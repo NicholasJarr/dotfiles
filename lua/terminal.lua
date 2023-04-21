7d83feeee6386c0c3e75fa9d9575cd9c86298519
Nicholas Jarretta <nicholas@jarretta.com>
2023-04-21 14:18:53 -0300

    New test config

--- lua/terminal.lua
+++ lua/terminal.lua
@@ -1,5 +1,9 @@
 require("toggleterm").setup {
-  open_mapping = [[<c-\>]],
+  open_mapping = [[\<leader>]],
+  direction = 'vertical',
+  size = function(term)
+    return vim.o.columns * 0.3
+  end
 }
 
 vim.api.nvim_set_keymap('t', '<esc>', '<c-\\><c-n>', { noremap = true })
@@ -13,6 +17,31 @@ function create_augroup(autocmds, name)
   vim.cmd('augroup END')
 end
 
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+end
+
 create_augroup({
   { 'TermOpen', '*', 'nnoremap <buffer> . i<Up><cr><c-\\><c-n>G' },
+  { 'TermOpen', 'term://*', 'lua set_terminal_keymaps()' },
 }, 'terminal_commands')
+
+function rspec_test_line()
+  local path = vim.fn.expand('%')
+  local lnum = vim.fn.line('.')
+  return require('toggleterm').exec(('bin/rspec %s:%d'):format(path, lnum), 1)
+end
+
+function rspec_test_file()
+  local path = vim.fn.expand('%')
+  return require('toggleterm').exec(('bin/rspec %s'):format(path), 1)
+end
+
+create_augroup({
+  { 'BufEnter', '*.rb', [[nnoremap <buffer> \\ <cmd>lua rspec_test_line()<CR>]] },
+  { 'BufEnter', '*.rb', [[nnoremap <buffer> \[ <cmd>lua rspec_test_file()<CR>]] },
+}, 'ruby_terminal_commands')
